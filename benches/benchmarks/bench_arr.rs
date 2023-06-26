3e9cc2d278d9f82b480867d74a7c861255af8436
Marvin Hansen <marvin.hansen@gmail.com>
2023-06-26 13:54:47 +0800

    Updated benchmarks to use new implementation.

--- benches/benchmarks/bench_arr.rs
+++ benches/benchmarks/bench_arr.rs
@@ -1,7 +1,8 @@
 use criterion::{Criterion, criterion_group};
 
-use sliding_window::array_backed::SlidingWindow;
 use generic_array::typenum::U1000;
+use sliding_window::sliding_window::{new_with_generic_array_storage, SlidingWindow};
+use sliding_window::storage_gen_arr::GenericArrayStorage;
 
 use crate::benchmarks::fields::{SIZE};
 
@@ -10,17 +11,14 @@ pub struct Data {
     dats: i32,
 }
 
-impl Default for &Data
-{
-    fn default() -> Self {
-        &Data { dats: 0 }
-    }
+fn get_sliding_window() -> SlidingWindow<GenericArrayStorage<Data, U1000>, Data> {
+    new_with_generic_array_storage(SIZE)
 }
 
 fn array_backed_benchmark(criterion: &mut Criterion)
 {
-    let d1 = Data{dats:0};
-    let mut w: SlidingWindow<Data, U1000> = SlidingWindow::new(SIZE);
+    let d1 = Data { dats: 0 };
+    let mut w = get_sliding_window();
 
     criterion.bench_function("array_push", |bencher| {
         bencher.iter(||
--- benches/benchmarks/bench_vec.rs
+++ benches/benchmarks/bench_vec.rs
@@ -1,26 +1,28 @@
 
 use criterion::{Criterion, criterion_group};
-use sliding_window::vector_backed::SlidingWindow;
+use sliding_window::sliding_window::{new_with_vector_storage, SlidingWindow};
+use sliding_window::storage_vec::VectorStorage;
 use crate::benchmarks::fields::{MULT, SIZE};
 
-#[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
+#[derive(Default, Debug, Copy, Clone, Hash, Eq, PartialEq)]
 pub struct Data {
     dats: i32,
 }
-
+fn get_sliding_window() -> SlidingWindow<VectorStorage<Data>, Data> {
+    new_with_vector_storage(SIZE, MULT)
+}
 
 fn vector_backed_benchmark(criterion: &mut Criterion)
 {
     let d1 = Data{dats:0};
-    let mut w: SlidingWindow<Data> = SlidingWindow::new(SIZE,MULT);
+    let mut w = get_sliding_window();
 
     criterion.bench_function("vector_push", |bencher| {
         bencher.iter(||
-            w.push(&d1)
+            w.push(d1)
         )
     });
 }
-
 
 criterion_group! {
     name = vector_backed;
